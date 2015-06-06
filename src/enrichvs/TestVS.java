47d684a3068621123af74995ba53aa58684d3e96
Syed Asad Rahman <s9asad@gmail.com>
2015-06-06 23:25:17 +0100

    data file name changed

--- src/enrichvs/TestVS.java
+++ src/enrichvs/TestVS.java
@@ -23,7 +23,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package statistics;
+package enrichvs;
 
 import java.io.BufferedReader;
 import java.io.File;
--- src/enrichvs/VirtualScreening.java
+++ src/enrichvs/VirtualScreening.java
@@ -23,7 +23,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package statistics;
+package enrichvs;
 
 import java.util.Arrays;
 import java.util.LinkedList;
